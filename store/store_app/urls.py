456f547abe31cf846483b97eabcffbb3d61c506c
avani <avani6711@gmail.com>
2026-06-26 11:33:40 +0530

    created

---
+++ store/store_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('about/', about, name='about'),
+    path('blog-detail/', blog_detail, name='blog-detail'),
+    path('blog/', blog, name='blog'),
+    path('contact/', contact, name='contact'),
+    path('', home_02, name='home-02'),
+    path('home-03/', home_03, name='home-03'),
+    path('index/', index, name='index'),
+    path('product-detail/', product_detail, name='product-detail'),
+    path('product/', product, name='product'),
+    path('shopping-cart/', shoping_cart, name='shopping-cart'),
+]
